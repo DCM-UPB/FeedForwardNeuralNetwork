f7dbeddba8cb929fcfd1fb271a9d0959142bf641
francesco086 <francesco086@gmail.com>
2018-03-22 13:45:24 +0100

    activation functions now have a ideal input mu and sigma nad an output mu and sigma

--- src/ActivationFunctionInterface.hpp
+++ src/ActivationFunctionInterface.hpp
@@ -14,6 +14,16 @@ public:
     //return a 3-characters identification string
     virtual std::string getIdCode() = 0;
 
+    // return the ideal input mean value (mu) and standard deviation (sigma)
+    // (pretending a gaussian distribution)
+    virtual double getIdealInputMu() = 0;
+    virtual double getIdealInputSigma() = 0;
+
+    // return the output mean value (mu) and standard deviation (sigma)
+    // (pretending a gaussian distribution)
+    virtual double getOutputMu() = 0;
+    virtual double getOutputSigma() = 0;
+
     // compute the activation function value
     virtual double f(const double &) = 0;
 
--- src/GaussianActivationFunction.hpp
+++ src/GaussianActivationFunction.hpp
@@ -15,6 +15,13 @@ public:
 
     std::string getIdCode(){return "gss";}
 
+    // input should be in the rage [-3 : 3]
+    double getIdealInputMu(){return 0.;}
+    double getIdealInputSigma(){return ;}
+
+    double getOutputMu(){return ;}
+    double getOutputSigma(){return ;}
+
     double f(const double &in);
 
     double f1d(const double &in);
