cb225c917234e4cd8d49737183fa42bf4720f839
francesco086 <francesco086@gmail.com>
2018-03-22 13:45:24 +0100

    activation functions now have a ideal input mu and sigma nad an output mu and sigma

--- src/IdentityActivationFunction.hpp
+++ src/IdentityActivationFunction.hpp
@@ -14,6 +14,15 @@ public:
     ~IdentityActivationFunction(){}
 
     std::string getIdCode(){return "id_";}
+
+    // input can be assumed to be in the rage [-1 : 1] -> mu=0   sigma=1/sqrt(3)
+    double getIdealInputMu(){return 0.;}
+    double getIdealInputSigma(){return 0.577350269189626;}
+
+    // output is identical to the input
+    double getOutputMu(){return 0.;}
+    double getOutputSigma(){return 0.577350269189626;}
+
     double f(const double &in){return in;}
     double f1d(const double &in){return 1.;}
     double f2d(const double &in){return 0.;}
--- src/LogisticActivationFunction.hpp
+++ src/LogisticActivationFunction.hpp
@@ -14,6 +14,14 @@ public:
 
     std::string getIdCode(){return "lgs";}
 
+    // input should be in the rage [-5 : 5] -> mu=0   sigma=sqrt(3)
+    double getIdealInputMu(){return 0.;}
+    double getIdealInputSigma(){return 2.886751345948129;}
+
+    // output is in the range [0 : 1] -> mu=0.5   sigma=1/(2*sqrt(3))
+    double getOutputMu(){return 0.5;}
+    double getOutputSigma(){return 0.288675134594813;}
+
     double f(const double &in);
 
     double f1d(const double &in);
